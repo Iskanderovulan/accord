6ca9f15dc6a15c1b57ccca24a4a028ac253cdb29
Emma Paris <email@gmail.com>
2024-03-19 18:12:05 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const accordionData = [
+  {
+    title: "first",
+    content: "a;lskdla;skd;laskdasd",
+  },
+  {
+    title: "second",
+    content: "a;lskdla;skd;laskdasd",
+  },
+  {
+    title: "first",
+    content: "a;lskdla;skd;laskdasd",
+  },
+];
